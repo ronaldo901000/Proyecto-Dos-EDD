88ec82fcedb4cf1d04fbccd9ba4bc8b62d507073
Ronaldo Hilario <ronaldohilario901000@gmail.com>
2026-04-11 16:31:59 -0600

    creacion del proyecto

---
+++ Gestor de Supermecado /Codigo Fuente/GestorSupermercado/src/main/java/com/ronaldo/gestor/Main.java
@@ -0,0 +1,12 @@
+package com.ronaldo.gestor;
+
+/**
+ *
+ * @author ronaldo
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
